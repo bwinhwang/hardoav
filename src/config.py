26f5adb49384ff6a82f6b934c5a1a694c81f843e
Jason Wang <bwinhwang@gmail.com>
2018-02-27 20:38:21 +0800

    set output dir

--- src/config.py
+++ src/config.py
@@ -2,7 +2,6 @@
 import os.path
 WORKER_COUNT = 1
 TOPIC_NUM = 3*10
-# OUTPUT_DIR = os.path.expanduser("~/hardoav_downloads")
-OUTPUT_DIR = "/media/sda8/hardoav_downloads"
+OUTPUT_DIR = os.path.expanduser("~/hardoav_downloads")
 SITE = "http://t66y.com/"
 
